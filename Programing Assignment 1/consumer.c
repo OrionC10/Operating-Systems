2873ba5558709e2eb47069ced383368b91985a44
Matt Crosby <131556132+OrionC10@users.noreply.github.com>
2024-04-05 21:26:50 -0400

    Update consumer.c

--- Programing Assignment 1/consumer.c
+++ Programing Assignment 1/consumer.c
@@ -1,3 +1,9 @@
+//consumer.c
+//    This is the consumer in the producer consumer problem.
+//    This is will take a char on the shared memory table and "consume" it.
+//Matthew Crosby
+//4/5/2024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,10 +11,10 @@
 #include <sys/mman.h>
 #include <semaphore.h>
 
-#define BUFFER_SIZE 2
+#define TABLE_SIZE 2
 
 struct shared_data {
-    char buffer[BUFFER_SIZE];
+    char buffer[TABLE_SIZE];
     sem_t mutex, full, empty;
 };
 
@@ -16,7 +22,7 @@ int main() {
     int shm_fd;
     struct shared_data *shared_memory;
 
-    // Open shared memory
+    // Open shared memory table
     shm_fd = shm_open("/myshm", O_RDWR, 0666);
     shared_memory = (struct shared_data *) mmap(NULL, sizeof(struct shared_data), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
@@ -26,7 +32,7 @@ int main() {
         sem_wait(&shared_memory->full);
         sem_wait(&shared_memory->mutex);
 
-        item = shared_memory->buffer[i % BUFFER_SIZE];
+        item = shared_memory->buffer[i % TABLE_SIZE];
         printf("Consumed item: %c\n", item);
 
         sem_post(&shared_memory->mutex);
